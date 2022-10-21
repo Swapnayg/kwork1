dd74b2e4ddac7301f49ec902515a755d0354ab3b
Swapnayg <infoswap90@gmail.com>
2022-10-22 00:04:14 +0530

    commit

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py makemigrations kworkapp && python manage.py makemigrations && python manage.py migrate
-web: uvicorn mainKwork.asgi:application --port $PORT --host 0.0.0.0
-worker: python manage.py runworker channel_layer
+web: daphne mainKwork.asgi:application --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker channel_layer --settings=mainKwork.settings -v2
--- mainKwork/asgi.py
+++ mainKwork/asgi.py
@@ -7,25 +7,28 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainKwork.settings')
+django.setup()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainKwork.settings")
-asgi_application = get_asgi_application()
+import kworkapp.routing
 
-from channels.auth import AuthMiddlewareStack
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+django_asgi_app = get_asgi_application()
 
-from kworkapp import routing
-
-application = ProtocolTypeRouter(
-    {
-        "http": asgi_application,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            kworkapp.routing.websocket_urlpatterns
+        )
+    )
+})
